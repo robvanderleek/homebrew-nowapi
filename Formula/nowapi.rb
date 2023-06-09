f5f2805aa37c0b0f27728e7c3e3ef1f4d30c1ef3
Rob van der Leek <robvanderleek@gmail.com>
2023-06-09 22:27:23 +0200

    Updated

--- Formula/nowapi.rb
+++ Formula/nowapi.rb
@@ -3,8 +3,8 @@
 class Nowapi < Formula
   desc "When you need an API Now."
   homepage "https://github.com/robvanderleek/nowapi"
-  url "https://github.com/robvanderleek/nowapi/releases/download/v1.1.0/nowapi-macos.tar.gz"
-  sha256 "7116c20094f450fa35d290d9ea3b885d14e634044306d87f6084fffc273ac0db"
+  url "https://github.com/robvanderleek/nowapi/releases/download/v1.2.0/nowapi-macos.tgz"
+  sha256 "e722835e68fe815285ae53bc6fade0d7dcbe0d1320049dc61fe77c29b0eef291"
   license "MIT"
   version "1.1.0"
 
