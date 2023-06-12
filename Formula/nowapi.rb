fe2d02a3c0090594c1ec4f2f22b6226100fbc0e7
Rob van der Leek <robvanderleek@gmail.com>
2023-06-12 08:49:57 +0200

    nowapi 1.11.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/nowapi.rb
+++ Formula/nowapi.rb
@@ -3,10 +3,10 @@
 class Nowapi < Formula
   desc "When you need an API Now."
   homepage "https://github.com/robvanderleek/nowapi"
-  url "https://github.com/robvanderleek/nowapi/releases/download/1.10.0/nowapi-macos.tgz"
-  sha256 "437b17e3b4e71f995d707cb578a732aaf8ed7b3cc5a727ef34d4ed1c5b599ae0"
+  url "https://github.com/robvanderleek/nowapi/releases/download/1.11.0/nowapi-macos.tgz"
+  sha256 "071e694c6efccb74ef1bc4c120159220472e674d2258f7b0e5a55a0cf36d703e"
   license "MIT"
-  version "1.10.0"
+  version "1.11.0"
 
   # depends_on "cmake" => :build
 
